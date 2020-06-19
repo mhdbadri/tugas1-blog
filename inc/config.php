cc93972557c1684686159b5b928174f5a3a526f3
Mhd Badri <66723139+mhdbadri@users.noreply.github.com>
2020-06-19 11:48:13 +0700

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 session_start();
 
 
-define('URL', 'http://localhost/sigadgetfix/');
+define('URL', 'http://localhost/tugas1-blog/');
 define('ASSET' , URL . 'layout/assets/');
 require_once "vendor/autoload.php";
 
